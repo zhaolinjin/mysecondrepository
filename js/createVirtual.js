4fc18da490e870b73e03040be5c3a19c2764ba59
zhaolinjin <mrzhaolinjin@sina.com>
2017-07-10 21:21:14 +0800

    710wan

--- js/createVirtual.js
+++ js/createVirtual.js
@@ -12,30 +12,7 @@ $(document).ready(function () {
                 if ($("#createVirtual .step li").length > 1) {
                     $($($("#createVirtual .step li.active"))[$("#createVirtual .step li.active").length - 1]).removeClass("active")
                 }
-            };
-            // if($($(".breedList").children("li")[1]).hasClass("active")){
-            //     $(".MN-pre").show();
-            //     $(".tipFirst").hide();
-            //     $(".tipSecond").hide();
-            // }else{
-            //     $(".MN-pre").hide();
-            //     $(".tipFirst").hide();
-            //     $(".tipSecond").hide();
-            // };
-            // if($($(".breedList").children("li")[3]).hasClass("active")){
-            //     $(".MN-pre").show();
-            //     $(".tipFirst").show();
-            // };
-            // if($($(".breedList").children("li")[4]).hasClass("active")){
-            //     $("#createEnsureBtn").show();
-            //     $(".MN-next").hide();
-            //     $(".tipFirst").hide();
-            //     $(".tipSecond").show();
-            // }else{
-            //     $("#createEnsureBtn").hide();
-            //     $(".MN-next").show();
-            //     $(".tipSecond").hide();
-            // };
+            }
         })
     });
 })
